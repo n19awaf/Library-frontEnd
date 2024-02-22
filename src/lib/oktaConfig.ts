b9633091f3dc369700059d385b2636d58b855068
N19awaf <nawaf13-@hotmail.com>
2024-02-22 18:40:08 +0800

    last updated

---
+++ src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: '0oafap3yattMrzKEB5d7',
+    issuer: 'https://dev-35378216.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true,
+}
