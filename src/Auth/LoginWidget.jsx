83978140e1ed872c7051f9c5b2f8327a18f6f82b
N19awaf <nawaf13-@hotmail.com>
2024-02-23 14:18:54 +0800

    last updated

--- src/Auth/LoginWidget.jsx
+++ src/Auth/LoginWidget.jsx
@@ -1,6 +1,7 @@
 import { Redirect } from "react-router-dom";
 import { useOktaAuth } from "@okta/okta-react";
 import { SpinnerLoading } from "../layouts/Utils/SpinnerLoading";
+import OktaSignInWidget from "./OktaSigninWidget";
 
 const LoginWedget = ({config}) => {
     const {oktaAuth, authState} = useOktaAuth();
@@ -19,7 +20,7 @@ const LoginWedget = ({config}) => {
     return authState.isAuthenticated ?
     <Redirect to= {{ pathname:'/'}}/>
     :
-    <div></div>
+    <OktaSignInWidget config={config} onSuccess={onSuccess} onError={onError}/>
 }
 
 export default LoginWedget;
