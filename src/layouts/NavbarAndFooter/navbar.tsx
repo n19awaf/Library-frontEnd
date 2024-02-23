bb8f2d5a42a66120361842296e22fa7c8d6604c1
N19awaf <nawaf13-@hotmail.com>
2024-02-23 20:16:34 +0800

    last updated

--- src/layouts/NavbarAndFooter/navbar.tsx
+++ src/layouts/NavbarAndFooter/navbar.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useOktaAuth } from "@okta/okta-react";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 export const Navbar = () => {
@@ -11,7 +11,7 @@ export const Navbar = () => {
 
     const handleLogout = async () => oktaAuth.signOut();
     console.log(authState);
-    
+
     return(
         <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
         <div className="container-fluid">
@@ -30,9 +30,16 @@ export const Navbar = () => {
                 </li>
             </ul>
             <ul className="navbar-nav ms-auto">
-                <li className="nav-item m-1">
-                <a type='button' href='#' className='btn btn-outline-light'>Sign in</a>
+                {!authState.isAuthenticated ?
+                    <li className="nav-item m-1">
+                    <Link type='button' to='/login' className='btn btn-outline-light'>Sign in</Link>
+                    </li>
+                :
+                <li>
+                    <button className="btn btn-outline-light" onClick={handleLogout}>Logout</button>
                 </li>
+                }
+                
             </ul>
             </div>
         </div>
