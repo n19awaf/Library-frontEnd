e3161bbc6b89294af87cc460040060fdaa90729c
N19awaf <nawaf13-@hotmail.com>
2024-02-23 16:12:31 +0800

    last updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,27 @@ import { Navbar } from './layouts/NavbarAndFooter/navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
+import { oktaConfig } from './lib/oktaConfig';
+import {OktaAuth, toRelativeUrl} from '@okta/okta-auth-js';
+
+
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 export const App = () =>{
+
+  const customAuthHandler = () => {
+    history.push('/login');
+  }
+  const history = useHistory();
+
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin))
+  };
+
+
   return (
 
     //D-flex: يشير إلى استخدام CSS Flexbox لترتيب عناصر الواجهة أفقيًا أو رأسيًا. Flexbox هو نمط تخطيط مرن يسمح بترتيب وتنظيم عناصر الواجهة بطريقة سهلة ومرنة.
