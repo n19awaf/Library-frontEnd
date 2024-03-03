3f844eaded7f7a73cee38a59b7ae122a35373d8e
N19awaf <nawaf13-@hotmail.com>
2024-03-04 04:53:12 +0800

    last updated

--- src/layouts/Utils/LeaveAReview.tsx
+++ src/layouts/Utils/LeaveAReview.tsx
@@ -4,9 +4,12 @@ import { StarsReview } from "./StarsReview";
 export const LeaveAReview: React.FC<{}> = (props) => {
 
     const [starInput, setStarInput] = useState(0);
+    const [displayInput, setDisplayInput] = useState(false);
+    const [reviewDescription, setReviewDescription] = useState('');
 
     function starValue(value: number) {
         setStarInput(value);
+        setDisplayInput(true);
     }
 
     return(
@@ -28,6 +31,20 @@ export const LeaveAReview: React.FC<{}> = (props) => {
                 <li><button onClick={() => starValue(5)} className="dropdown-item">5 star</button></li>
             </ul>
             <StarsReview rating={starInput} size={32}/>
+
+            {displayInput && 
+            <form action="#" method="POST">
+                <hr />
+                <div className="mb-3">
+                    <label htmlFor="" className="form-label">Description</label>
+                    <textarea name="" id="submitReviewDescription" className="form-control" placeholder="Optional"
+                        rows={3} onChange={e => setReviewDescription(e.target.value)}></textarea>
+                </div>
+                <div>
+                    <button type="button" className="btn btn-primary mt-3">Submit Review</button>
+                </div>
+            </form>
+            }
         </div>
         
     );
