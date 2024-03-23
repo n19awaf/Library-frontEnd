d2231010f83d090fd25dde2bf91558f77d395aaf
N19awaf <nawaf13-@hotmail.com>
2024-03-23 08:33:45 +0800

    last updated

--- src/layouts/ShelfPage/ShelfPage.tsx
+++ src/layouts/ShelfPage/ShelfPage.tsx
@@ -1,19 +1,24 @@
+import { useState } from "react";
 import { HistoryPage } from "./componesnts/HistoryPage";
 import { Loans } from "./componesnts/Loans";
 
 export const ShelfPage = () => {
+
+    //going to force a new use effect upload each time the history page opens up
+    const [historyClick, setHistoryClick] = useState(false);
+
 
     return(
         <div className="container">
             <div className="mt-3">
                 <nav>
                     <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                        <button className="nav-link active" id="nav-loans-tab" data-bs-toggle="tab"
+                        <button onClick={() => setHistoryClick(false)} className="nav-link active" id="nav-loans-tab" data-bs-toggle="tab"
                             data-bs-target="#nav-loans" type="button" role="tab" aria-controls="nav-loans"
                                 aria-selected="true">
                                     Loans
                         </button>
-                        <button className="nav-link" id="nav-history-tab" data-bs-toggle="tab"
+                        <button onClick={() => setHistoryClick(true)} className="nav-link" id="nav-history-tab" data-bs-toggle="tab"
                                 data-bs-target="#nav-history" type="button" role="tab" aria-controls="nav-loans"
                                 aria-selected="true">
                                     Your History
@@ -26,7 +31,7 @@ export const ShelfPage = () => {
                             <Loans/>
                     </div>
                     <div className="tab-pane fade" id="nav-history" role="tabpanel" aria-labelledby="nav-history-tab">
-                        <HistoryPage/>
+                        { historyClick ? <HistoryPage/> : <></>}
                     </div>
                 </div>
             </div>
