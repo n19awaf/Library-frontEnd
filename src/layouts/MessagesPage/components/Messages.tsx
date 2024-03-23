473457b387ffaf91537c1111dafdcbaafc544b19
N19awaf <nawaf13-@hotmail.com>
2024-03-24 01:41:09 +0800

    last updated

--- src/layouts/MessagesPage/components/Messages.tsx
+++ src/layouts/MessagesPage/components/Messages.tsx
@@ -1,5 +1,8 @@
 import { useOktaAuth } from "@okta/okta-react";
-import { useState } from "react";
+import { error } from "console";
+import { useEffect, useState } from "react";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+import MessageModel from "../../../models/MessageModel";
 
 export const Messages = () => {
 
@@ -8,9 +11,56 @@ export const Messages = () => {
     const [httpError, setHttpError] = useState(null);
 
     //Messages
+    const [messages, setMessages] = useState<MessageModel[]>([])
+    //Pagination
     const [messagesPerPage] = useState(5);
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(0);
+
+    useEffect(() => {
+        const fetchUseMessages = async () => {
+            if(authState && authState?.isAuthenticated) {
+                const url = `http://localhost:8080/api/messages/search/findByUserEmail/?userEmail=${authState?.accessToken?.claims.sub}&page=${currentPage - 1}&size=${messagesPerPage}`;
+                const requestOptions = {
+                    method:'GET',
+                    headers:{
+                        Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                        'Content-type':'application/json'
+                    },
+                };
+                const messagesResponse = await fetch(url, requestOptions);
+                if (!messagesResponse.ok) {
+                    throw new Error("Something went Wrong");
+                }
+                const messagesResponseJson = await messagesResponse.json();
+                setMessages(messagesResponseJson._embedded.message);
+                setTotalPages(messagesResponseJson.page.totalPages);
+            }
+            setIsLoadingMessages(false);
+
+        }
+        fetchUseMessages().catch((error: any) => {
+            setIsLoadingMessages(false);
+            setHttpError(error.message);
+        })
+        window.scroll(0, 0);
+    },[authState, currentPage])
+
+    if (isLoadingMessage) {
+        return(
+            <SpinnerLoading/>
+        );
+    }
+
+    if (httpError) {
+        return(
+            <div className="container m-5">
+                <p>{httpError}</p>
+            </div>
+        );
+    }
+
+    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
 
 
