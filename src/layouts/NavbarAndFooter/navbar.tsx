963d54a2f19eb99a8af040ca32129fdee1913ce7
N19awaf <nawaf13-@hotmail.com>
2024-03-28 07:57:34 +0800

    last updated

--- src/layouts/NavbarAndFooter/navbar.tsx
+++ src/layouts/NavbarAndFooter/navbar.tsx
@@ -33,6 +33,12 @@ export const Navbar = () => {
                         <NavLink to="/shelf" className="nav-link">Shelf</NavLink>
                     </li>
                 }
+                {authState.isAuthenticated && authState.accessToken?.claims.userType === 'admin' &&
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/admin">Admin</NavLink>
+                    </li>
+                }
+
             </ul>
             <ul className="navbar-nav ms-auto">
                 {!authState.isAuthenticated ?
