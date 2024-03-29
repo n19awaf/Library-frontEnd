0f65430581119b4852cf6410c7739ae9a41ae511
N19awaf <nawaf13-@hotmail.com>
2024-03-29 15:14:03 +0800

    last updated

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oafap3yattMrzKEB5d7',
     issuer: 'https://dev-35378216.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'https://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
