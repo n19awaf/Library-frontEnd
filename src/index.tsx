26b4a4d706cce56dc7fb978a522190af7cc4c473
N19awaf <nawaf13-@hotmail.com>
2024-03-30 15:36:22 +0800

    last updated

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
 import {BrowserRouter} from 'react-router-dom'
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 
+const stripePromise = loadStripe('pk_test_51OzaHlD3qVhmZEFqgIec4p65HKBDnitPW7ED7oMMbpMCkZDj3TXrXtiElNv6EFhzD5blCTpuLC12O2UqPZkwu1Qr00R9fGOwnU');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <App />
+  <Elements stripe={stripePromise}>
+      <App />
+  </Elements>
   </BrowserRouter>
 );
 
