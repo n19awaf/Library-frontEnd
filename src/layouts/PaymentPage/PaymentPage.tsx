1c863224bcac81d1b3a54c204e253e78da389dd6
N19awaf <nawaf13-@hotmail.com>
2024-03-31 04:52:21 +0800

    last updated

--- src/layouts/PaymentPage/PaymentPage.tsx
+++ src/layouts/PaymentPage/PaymentPage.tsx
@@ -1,5 +1,5 @@
 import { useOktaAuth } from "@okta/okta-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 
 export const PaymentPage = () => {
@@ -10,6 +10,20 @@ export const PaymentPage = () => {
     const [submitDisabled, setSubmitDisabled] = useState(false);
     const [fees,setFees] = useState(0);
     const [loadingFees, setLoadingFees] = useState(true);
+
+    useEffect(() => {
+        const fetchFees = async() => {
+            if (authState && authState.isAuthenticated) {
+                const url = `${process.env.REACT_APP_API}/payments/search/findByUserEmail?userEmail=${authState.accessToken?.claims.sub}`;
+                const requestOptions = {
+                    method:'GET',
+                    headers:{
+                        'Content-type':'application/json'}
+                };
+                const paymentResponse = await fetch(url, requestOptions);
+            }
+        }
+    })
 
     if (loadingFees) {
         <SpinnerLoading/>
